faee422fc6c2936c8e53a5be2ce772a01fa56157
Azoula Arthur <arthur.azoula@epitech.eu>
2024-04-29 13:56:30 +0200

    REFACTOR: change url from photo on product to get random picture of clothing

--- app/src/DataFixtures/ProductFixtures.php
+++ app/src/DataFixtures/ProductFixtures.php
@@ -10,15 +10,15 @@ use Doctrine\Persistence\ObjectManager;
 
 class ProductFixtures extends Fixture
 {
+    
     public function load(ObjectManager $manager): void
     {
-        for($i = 0; $i < 50; $i++)
-        {
+        for ($i = 0; $i < 1000; $i++) {
             $prd = new Product();
             $prd->setName('Product ' . $i);
             $prd->setPrice(mt_rand(10, 100));
             $prd->setDescription('Description of product ' . $i);
-            $prd->setPhoto('https://via.placeholder.com/150');
+            $prd->setPhoto('https://source.unsplash.com/random/?clothing');
             $prd->setCategory(Category::getRandom());
             $prd->setGenre(Genre::getRandom());
             $manager->persist($prd);
