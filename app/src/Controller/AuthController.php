0d52608fffc95cee40d4ca368eafd4e9c647f6a4
Azoula Arthur <arthur.azoula@epitech.eu>
2024-04-30 10:49:42 +0200

    FEAT: Add admin routes to check if user is admin, add route "/me" to get user connected

--- app/src/Controller/AuthController.php
+++ app/src/Controller/AuthController.php
@@ -10,15 +10,18 @@ use App\Utils\Response;
 use Exception;
 use Lexik\Bundle\JWTAuthenticationBundle\Services\JWTTokenManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Bundle\SecurityBundle\Security;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Validator\Validator\ValidatorInterface;
 use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 
-class AuthController extends AbstractController {
-  
+class AuthController extends AbstractController
+{
+
   #[Route('/login', name: 'login', methods: ['POST'])]
-  public function login(Request $request, ClientProvider $clientProvider, ValidatorInterface $validator, JWTTokenManagerInterface $JWTManager): Response {
+  public function login(Request $request, ClientProvider $clientProvider, ValidatorInterface $validator, JWTTokenManagerInterface $JWTManager): Response
+  {
     try {
       $params = json_decode($request->getContent(), true);
 
@@ -31,30 +34,30 @@ class AuthController extends AbstractController {
       if (!password_verify($params["password"], $client->getPassword())) {
         return Response::error("Mot de passe incorrect", HttpStatus::BAD_REQUEST);
       }
-      
+
       $client->setRoles(['IS_AUTHENTICATED_FULLY']);
 
       $token = $JWTManager->create($client);
 
       return Response::json(['token' => $token]);
-
     } catch (Exception $e) {
       return Response::error($e->getMessage(), HttpStatus::INTERNAL_SERVER_ERROR);
     }
   }
-  
+
   #[Route('/register', name: 'register', methods: ['POST'])]
-  public function register(Request $request, ClientService $clientService, UserPasswordHasherInterface $passwordHasher): Response {
+  public function register(Request $request, ClientService $clientService, UserPasswordHasherInterface $passwordHasher): Response
+  {
     try {
       $params = json_decode($request->getContent(), true);
-      
+
       $verifyClientExist = $clientService->getClientByEmail($params["email"]);
       if ($verifyClientExist) {
         return Response::error("Cette adresse mail existe déjà", HttpStatus::BAD_REQUEST);
       }
 
       $plainedTextPassword = $params["password"];
-      
+
       $client = new Client();
       $client->setEmail($params["email"]);
       $client->setLogin($params["login"]);
@@ -65,21 +68,47 @@ class AuthController extends AbstractController {
         $client,
         $plainedTextPassword
       );
-      
+
       $client->setPassword($hashedPassword);
-      
+
       $clientService->create($client);
-      
+
       return Response::success("Votre compte a bien été crée !", HttpStatus::CREATED);
     } catch (Exception $e) {
       return Response::error($e->getMessage(), HttpStatus::INTERNAL_SERVER_ERROR);
     }
   }
-  
+
+  #[Route('/me', name: 'auth_me', methods: ['GET'])]
+  public function getUserConnected(ClientService $clientService): Response
+  {
+    try {
+
+      $mail = $this->getUser()->getUserIdentifier();
+
+      $client = $clientService->getClientByEmail($mail);
+
+      if (!$client) {
+        return Response::error("Cet utilisateur n'existe pas", HttpStatus::NOT_FOUND);
+      }
+
+      return Response::json($client->jsonSerialize(), HttpStatus::OK);
+    } catch (Exception $e) {
+      return Response::error($e->getMessage(), HttpStatus::INTERNAL_SERVER_ERROR);
+    }
+  }
+
+  #[Route('/admin', name: 'admin', methods: ['GET'])]
+  public function admin(): Response
+  {
+    return Response::success("OK", HttpStatus::OK);
+  }
+
+
   #[Route('/forgot-password', name: 'forgot_password', methods: ['POST'])]
-  public function forgotPassword(Request $request): Response {
-    
+  public function forgotPassword(Request $request): Response
+  {
+
     return new Response(json_encode(['message' => 'Forgot Password Route']));
   }
-  
 }
