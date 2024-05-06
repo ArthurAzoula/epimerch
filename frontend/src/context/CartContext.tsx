2a6c6e9a099af3dc1630757e76b4fa287b5e90f0
Azoula Arthur <arthur.azoula@epitech.eu>
2024-05-06 16:43:39 +0200

    FEAT: add remove items and change quantity to shopping cart

--- frontend/src/context/CartContext.tsx
+++ frontend/src/context/CartContext.tsx
@@ -1,21 +1,23 @@
-import React, { useEffect, useContext, useState } from "react";
+import React, { useEffect, useState } from "react";
 import CartService from "../service/cart.service";
 import { CartItem } from "../service/cartitem.service";
-import { AuthContext } from "./AuthContext";
 
 export const CartContext = React.createContext<{
   cartItems: CartItem[] | null,
   refreshCartItems: () => Promise<void>,
   addProductToCart: (productId: string, quantity: number) => Promise<boolean>,
+  updateQuantity: (productId: string, quantity: number) => Promise<void>,
+  removeItem: (productId: string) => Promise<void>,
 }>({
   cartItems: null,
   refreshCartItems: async () => {},
   addProductToCart: async () => false,
+  updateQuantity: async () => {},
+  removeItem: async () => {},
 });
 
 export const CartContextProvider = ({children} : {children: JSX.Element}): JSX.Element => {
     const [cartItems, setCartItems] = useState<CartItem[] | null>(null);
-    const { user } = useContext(AuthContext);
     
     useEffect(() => {
         refreshCartItems();
@@ -23,27 +25,37 @@ export const CartContextProvider = ({children} : {children: JSX.Element}): JSX.E
     }, []);
     
     async function refreshCartItems() {
-        if (user?.cart?.id) {
-            const response = await CartService.getProductsFromCart(user.cart.id);
-            if (!("error" in response)) {
-                setCartItems(response);
-            }
+        const response = await CartService.getProductsFromCart();
+        if (!("error" in response)) {
+            setCartItems(response);
         }
     }
     
     async function addProductToCart(productId: string, quantity: number) {
-        if (user?.cart?.id) {
-            const response = await CartService.addProductToCart(user.cart.id, productId, quantity);
-            if (!("error" in response)) {
-                refreshCartItems();
-                return true;
-            }
+        const response = await CartService.addProductToCart(productId, quantity);
+        if (!("error" in response)) {
+            refreshCartItems();
+            return true;
         }
         return false;
     }
+
+    async function updateQuantity(productId: string, quantity: number) {
+        const response = await CartService.updateQuantity(productId, quantity);
+        if (!("error" in response)) {
+            refreshCartItems();
+        }
+    }
+
+    async function removeItem(productId: string) {
+        const response = await CartService.removeProductFromCart(productId);
+        if (!("error" in response)) {
+            refreshCartItems();
+        }
+    }
     
     return (
-        <CartContext.Provider value={{ cartItems, refreshCartItems, addProductToCart }}>
+        <CartContext.Provider value={{ cartItems, refreshCartItems, addProductToCart, updateQuantity, removeItem }}>
         {children}
         </CartContext.Provider>
     );
