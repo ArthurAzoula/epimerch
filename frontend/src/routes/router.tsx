ea13b040cb0d9f8f0b1e9cfc9bd6559115bd71e6
Olivier <olivier.pakosz@epitech.eu>
2024-05-06 15:57:59 +0200

    feat: Mise en relation de la page d'inscription avec le back

--- frontend/src/routes/router.tsx
+++ frontend/src/routes/router.tsx
@@ -1,28 +1,32 @@
-import { BrowserRouter as ReactRouter, Route, Routes} from 'react-router-dom';
-import routes from './config.routes';
-import { useContext } from 'react';
-import { AuthContext } from '../context/AuthContext';
+import { BrowserRouter as ReactRouter, Route, Routes } from "react-router-dom";
+import routes from "./config.routes";
+import { useContext } from "react";
+import { AuthContext } from "../context/AuthContext";
 
 const Router = () => {
-    const { user, isAdmin } = useContext(AuthContext);
-    
-    return (
-        <ReactRouter>
-            <Routes>
-                {routes
-                    .filter(({requiresGuest, requiresAuth, requiresAdmin}) => {
-                        if(requiresGuest && user) return false;
-                        if(requiresAuth && !user) return false;
-                        if(requiresAdmin && !isAdmin) return false;
-                        
-                        return true;
-                    })
-                    .map((route, index) => (
-                        <Route key={index} path={route.path} element={<route.component />} />
-                ))}
-            </Routes>
-        </ReactRouter>
-    );
+  const { user, isAdmin } = useContext(AuthContext);
+
+  return (
+    <ReactRouter>
+      <Routes>
+        {routes
+          .filter(({ requiresGuest, requiresAuth, requiresAdmin }) => {
+            if (requiresGuest && user) return false;
+            if (requiresAuth && !user) return false;
+            if (requiresAdmin && !isAdmin) return false;
+
+            return true;
+          })
+          .map((route, index) => (
+            <Route
+              key={index}
+              path={route.path}
+              element={<route.component />}
+            />
+          ))}
+      </Routes>
+    </ReactRouter>
+  );
 };
 
 export default Router;
